39c559fa1b3cc74d5feb44b841c176591cdf09df
Ali Rahimi <alrhim@amazon.com>
2020-11-20 12:16:26 -0800

    enable pullup on microphone pin

--- main/knocker.c
+++ main/knocker.c
@@ -10,7 +10,7 @@
 #define GPIO_OUTPUT_IO_0    18
 #define GPIO_OUTPUT_IO_1    19
 #define GPIO_INPUT_BUTTON     0
-#define GPIO_INPUT_MIC     5
+#define GPIO_INPUT_MIC     16
 #define ESP_INTR_FLAG_DEFAULT 0
 
 typedef enum { EV_PUSH } Event;
@@ -141,6 +141,7 @@ void app_main(void)
     io_conf.mode = GPIO_MODE_INPUT;
     //enable pull-up mode
     io_conf.pull_up_en = 1;
+    io_conf.pull_down_en = 0;
     gpio_config(&io_conf);
 
     io_conf.pin_bit_mask = 1ULL<<GPIO_INPUT_MIC;
@@ -148,8 +149,9 @@ void app_main(void)
     io_conf.intr_type = GPIO_INTR_POSEDGE;
     //set as input mode
     io_conf.mode = GPIO_MODE_INPUT;
-    //disable pull-up mode
-    io_conf.pull_up_en = 0;
+    //enable pull-up mode
+    io_conf.pull_up_en = 1;
+    io_conf.pull_down_en = 0;
     gpio_config(&io_conf);
 
     // create event queues and tasks.
